31747dc08ab4373861cd7a66d65adc24c72b7791
Tanulo <boti.lorant.ferenc.22@ady-nagyatad-hu>
2025-09-23 11:25:41 +0200

    09.23

---
+++ PEZ OOP/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace PEZ_OOP
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<PEZ> Cukorkak = new List<PEZ>();
+
+			Cukorkak.Add(new PEZ("eper", "Enderman", 12));
+
+
+		}
+	}
+}
