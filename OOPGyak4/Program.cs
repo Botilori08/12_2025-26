74d24a5284d97132c9efa1fc74ad2c3ad5c6ddaa
boti.lorant.ferenc.2 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2025-10-08 11:20:22 +0200

    10.08

---
+++ OOPGyak4/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace OOPGyak4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Muzliszelet muzliszelet = new Muzliszelet();
+            muzliszelet.Nev = "Barackos műzliszelet";
+            muzliszelet.Elfogyasztas();
+        }
+    }
+}
