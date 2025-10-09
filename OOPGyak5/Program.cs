9bd588284c53e0ab28758b54a024fcfc46ab14e1
Lóru <boti.lorant.ferenc.22@ady-nagyatad.hu>
2025-10-09 13:16:25 +0200

    10.09

---
+++ OOPGyak5/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace OOPGyak5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Október_23 oktober23 = new Október_23();
+
+            oktober23.Rendezo = "Kovács Máté";
+            oktober23.Nev = "Október 23 ünnepély";
+            oktober23.Zajlik();
+            oktober23.Szereplok = new List<string> { "Taki", "John Smith" };
+            Console.WriteLine(oktober23.ToString());
+
+
+        }
+    }
+}
