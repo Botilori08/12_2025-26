5bc063d9fd3e6dccaac947abf660c9c35c5adf83
boti.lorant.ferenc.2 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2025-10-14 11:25:46 +0200

    10.14

---
+++ OOPGyak5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace OOPGyak5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Monitor m1 = new Monitor("VGA");
+
+            Monitor m2 = new Monitor("SVGA", 14.0);
+
+            Monitor m3 = new Monitor(640, 480, 14);
+
+            Console.WriteLine(m2.atlo);
+
+            Console.WriteLine(m1.atlo);
+
+            Console.WriteLine(m3.felbontas);
+
+        }
+    }
+}
