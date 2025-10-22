6d642822092a3110fa6fabe9ec0f8b6730796314
boti.lorant.ferenc.2 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2025-10-22 11:25:22 +0200

    10.22

---
+++ Erettsegi_2025okt/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Erettsegi_2025okt
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] sorok = File.ReadAllLines("ut.txt");
+
+            List <Adatok> adatok = new List <Adatok>();
+
+            int teljesUt = int.Parse(sorok[0]);
+
+            for(int i = 1; i < sorok.Length; i++)
+            {
+                adatok.Add(new Adatok(sorok[i]));
+            }
+
+            Console.WriteLine("2.feladat");
+            Console.WriteLine();
+
+
+
+        }
+    }
+}
