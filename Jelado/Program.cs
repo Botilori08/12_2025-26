f2409e7b4ed88bd04469b33cae139cd8fd959e56
boti.lorant.ferenc.2 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2025-11-26 11:26:25 +0100

    11.26

---
+++ Jelado/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Jelado
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List <Adat> adatok = new List <Adat> ();
+
+			string[] sorok = File.ReadAllLines("jel.txt");
+		
+			sorok.ToList().ForEach(sor => adatok.Add(new Adat(sor)));
+	
+			adatok = sorok.ToList().Select(x => new Adat(x)).ToList();
+
+        }
+	}
+}
