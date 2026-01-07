764121447c4fa16d68b9b544ee46f1eddb019b81
boti.lorant.ferenc.2 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-01-07 11:25:25 +0100

    01.07

---
+++ operator_feluliras/Szam.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace operator_feluliras
+{
+    internal class Szam
+    {
+        public int szam;
+
+        public Szam(int szam) 
+        {
+            this.szam = szam;
+        }
+
+        public static Szam operator+(Szam szam1,Szam szam2)
+        {
+            return new Szam(szam1.szam + szam2.szam);
+        }
+
+        public static Szam operator+(Szam szam1,int szam2)
+        {
+            return new Szam(szam1.szam + szam2);
+        }
+        public static int operator +(int szam1, Szam szam2)
+        {
+            return szam1 + szam2.szam;
+        }
+
+        public static double operator +(Szam szam1, double szam2)
+        {
+            return (double)szam1.szam + szam2;
+        }
+
+
+        public override string ToString()
+        {
+            return "A szám értéke: "+szam;
+        }
+
+        public static Szam operator ++(Szam szam)
+        {
+            return new Szam(szam.szam+1);
+        }
+
+    }
+}
