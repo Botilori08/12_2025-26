a0ff564d7fde0101454e837e2f6630ade386e147
boti.lorant.ferenc.2 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-01-19 10:35:53 +0100

    01.19

--- Listás_01-16/Program.cs
+++ Listás_01-16/Program.cs
@@ -4,9 +4,9 @@
     {
         static void Main(string[] args)
         {
-            Szamok sz1 = new Szamok(new int[] { 1, 2, 3, 4, 5 });
+            Szamok sz1 = new Szamok(new int[] { 1, 2, 3, 4, 5, 2, 3 });
 
-            Szamok sz2 = new Szamok(new int[] { 2, 3, 6, 8 });
+            Szamok sz2 = new Szamok(new int[] { 2, 3, 6, 8, 2});
 
             Console.WriteLine(sz1+sz2);
 
@@ -14,6 +14,10 @@
             Console.WriteLine(sz1+200);
 
             Console.WriteLine(sz1+120+sz2);
+
+            Console.WriteLine(sz1-sz2);
+
+            Console.WriteLine(sz1-2);
 
         }
     }
