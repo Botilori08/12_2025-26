cc8ff1811e3d227106f42377e3270ae66ab93eb0
boti.lorant.ferenc.2 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-01-28 11:24:38 +0100

    01.28

---
+++ Shakespeare_regex/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Diagnostics.Contracts;
+using System.Text.RegularExpressions;
+
+namespace Shakespeare_regex
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string forras = File.ReadAllText("forras.txt");
+
+            Regex rg = new Regex(@"[A-ZÖÜÓŐÚÉÁŰÍ-]{2,}\s[A-ZÖÜÓŐÚÉÁŰÍ-]{2,}\b|[A-ZÖÜÓŐÚÉÁŰÍ-]{2,}\b");
+            var eredmeny = rg.Matches(forras);
+
+            /*Console.WriteLine(eredmeny.Count);
+            Console.WriteLine();*/
+
+            foreach (Match match in eredmeny)
+            {
+                Console.WriteLine(match.Value);
+            }
+
+        }
+    }
+}
