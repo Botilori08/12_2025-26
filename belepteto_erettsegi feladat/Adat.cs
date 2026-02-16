fe7bab0ef7a468b92e82f5b9922f22cf97672d91
boti.lorant.ferenc.2 <boti.lorant.ferenc.22@ady-nagyatad.hu>
2026-02-16 10:36:25 +0100

    02.16

---
+++ belepteto_erettsegi feladat/Adat.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace belepteto_erettsegi_feladat
+{
+    class Adat
+    {
+        public string kod;
+        public string ido;
+        public int esemenyKod;
+
+
+        public Adat(string kod, string ido, int esemenyKod)
+        {
+            this.kod = kod;
+            this.ido = ido;
+            this.esemenyKod = esemenyKod;
+        }
+
+        public Adat(string sor)
+        {
+            string[] vag = sor.Split(" ");
+
+            this.kod = vag[0];
+            this.ido = vag[1];
+            this.esemenyKod = Convert.ToInt32(vag[2]);
+        }
+    }
+}
---
+++ belepteto_erettsegi feladat/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace belepteto_erettsegi_feladat
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            betoltes();
+        }
+
+        void betoltes()
+        {
+            string[] sorok = File.ReadAllLines("bedat.txt");
+
+            List<Adat> adatok = new List<Adat>();
+
+            foreach (var sor in sorok)
+            {
+                adatok.Add(new Adat(sor));
+            }
+
+        }
+    }
+
+
+}
